21298a17d4fce335176b376feb05d4f8daeb8bf2
Cyprien Guillemot <github@guillemot.me>
2025-09-03 18:35:58 +0200

    refactor: clean useless depedencies

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,6 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-import remarkCollapse from "remark-collapse";
-import remarkToc from "remark-toc";
 import config from "./src/config";
 import mdx from "@astrojs/mdx";
 
@@ -12,15 +10,6 @@ export default defineConfig({
   site: config.hostname,
   integrations: [react(), sitemap(), mdx()],
   markdown: {
-    remarkPlugins: [
-      remarkToc,
-      [
-        remarkCollapse,
-        {
-          test: "Table of contents",
-        },
-      ],
-    ],
     shikiConfig: {
       theme: "one-dark-pro",
       wrap: true,
